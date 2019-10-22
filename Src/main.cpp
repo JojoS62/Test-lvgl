cca378f487cff7834e3d4080b31bd6df8d54729d
JojoS62 <sn2010@onlinehome.de>
2019-10-22 09:40:38 +0200

    removed unnecessary code, code formatting

--- Src/main.cpp
+++ Src/main.cpp
@@ -15,31 +15,32 @@ Thread threadLvHandler;
 Ticker tickerStepper;
 Ticker tickerLvgl;
 
-DigitalOut	led1(LED1);                 // onboard LEDs
-DigitalOut	led2(LED2);
+DigitalOut led1(LED1); // onboard LEDs
+DigitalOut led2(LED2);
 Adafruit_TFTLCD_16bit_STM32 tft(NC);
 
-// global vars for stepper motor 
+// global vars for stepper motor
 
 bool motorOn = false;
 int motorSpeed = 40;
-DigitalOut motorStep(PE_5);             // stepping pulse
-DigitalOut motorDirection(PE_2);        // stepping direction
-
+DigitalOut motorStep(PE_5);      // stepping pulse
+DigitalOut motorDirection(PE_2); // stepping direction
 
 //
 // stepper motor control
 //
 
-void fnStepper() {
+void fnStepper()
+{
     static int stepCount = 0;
 
-    if (!motorOn)
+    if (!motorOn) {
         return;
+    }
 
     if (stepCount++ >= motorSpeed) {
         stepCount = 0;
-         motorStep = !motorStep;
+        motorStep = !motorStep;
     }
 }
 
@@ -47,24 +48,24 @@ void fnStepper() {
 // lv Ticker, executed in interrupt context
 //
 
-void fnLvTicker ()
+void fnLvTicker()
 {
-    lv_tick_inc(2);      /*Tell LittlevGL that 2 milliseconds were elapsed*/
+    lv_tick_inc(2); /*Tell LittlevGL that 2 milliseconds were elapsed*/
 }
-
 
 //
 // IO Thread
 // control tft backlight by button K1
 
-void threadFnIO() {
+void threadFnIO()
+{
     DigitalIn inButtonK1(PE_3, PullUp);
 
     PwmOut outTftBL(PB_1);
     float delta = 0.1f;
 
-    outTftBL.period_ms(1);  // PWM freq 1kHz
-    outTftBL = 1.0f;  // Backlight init with 100 %
+    outTftBL.period_ms(1); // PWM freq 1kHz
+    outTftBL = 1.0f;       // Backlight init with 100 %
     int btnK1Old = inButtonK1.read();
 
     while (true) {
@@ -72,10 +73,11 @@ void threadFnIO() {
 
         if ((btnK1 != btnK1Old) && (btnK1 == 0)) {
             //outTftBL = !outTftBL;
-            if (outTftBL > 0.9f)
+            if (outTftBL > 0.9f) {
                 delta = -0.1f;
-            else if (outTftBL < 0.1f)
+            } else if (outTftBL < 0.1f) {
                 delta = 0.1f;
+            }
 
             outTftBL = outTftBL + delta;
         }
@@ -84,26 +86,6 @@ void threadFnIO() {
 
         ThisThread::sleep_for(10);
     }
-}
-
-//
-//  print raw and calibrated touch coordinates
-//   usess a modified lv_index_data_t, added raw coordinates
-//
-
-void testTouch() 
-{
-    while(1) {
-		led1 = !led1;
-        lv_indev_data_t touchData;
-        volatile bool isTouched = readTouchXPT2046(NULL, &touchData);
-
-        if  (isTouched) {
-            printf("x: %4d   y: %4d  x: %4d   y: %4d\n", touchData.raw.x, touchData.raw.y, touchData.point.x, touchData.point.y);
-        }
-
-		ThisThread::sleep_for(100);
-	}
 }
 
 //
@@ -119,13 +101,14 @@ void sleepWithLvHandler(uint32_t sleepTime_ms)
         led1 = !led1;
         lv_task_handler();
         ThisThread::sleep_for(timeSlice);
-        if (sleepTime_ms > 0)
+        if (sleepTime_ms > 0) {
             elapsedTime += timeSlice;
+        }
     }
 }
 
 // main() runs in its own thread in the OS
-int main() 
+int main()
 {
     printf("Hello from STM32F407VE\n");
 
@@ -134,24 +117,22 @@ int main()
 
     tft.fillScreen(RED);
 
-	initTouchXPT2046();
-
-    //testTouch();
+    initTouchXPT2046();
 
     lv_init();
     lv_port_disp_init();
 
     // setup touchpad
     lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);              // Basic initialization
-    indev_drv.type = LV_INDEV_TYPE_POINTER;     // touchpad
+    lv_indev_drv_init(&indev_drv);          // Basic initialization
+    indev_drv.type = LV_INDEV_TYPE_POINTER; // touchpad
     indev_drv.read_cb = readTouchXPT2046;
 
     /* Register the driver in LittlevGL and save the created input device object*/
-    lv_indev_t * my_indev = lv_indev_drv_register(&indev_drv);
+    lv_indev_t *my_indev = lv_indev_drv_register(&indev_drv);
 
     /* Set cursor */
-    lv_obj_t * mouse_cursor = lv_img_create(lv_disp_get_scr_act(NULL), NULL);
+    lv_obj_t *mouse_cursor = lv_img_create(lv_disp_get_scr_act(NULL), NULL);
     lv_img_set_src(mouse_cursor, LV_SYMBOL_PLUS);
     lv_indev_set_cursor(my_indev, mouse_cursor);
 
@@ -160,15 +141,14 @@ int main()
     tickerStepper.attach_us(&fnStepper, 10);
     tickerLvgl.attach_us(&fnLvTicker, 2000);
 
-	//lv_tutorial_hello_world();
+    //lv_tutorial_hello_world();
     //demo_create();
     //lv_test_theme_2();
     //benchmark_create();
-    
-	lvSplashScreen();
+
+    lvSplashScreen();
     sleepWithLvHandler(2000);
     lvParameterScreen();
 
-    sleepWithLvHandler(0);  // sleep forever and call lv_handler
+    sleepWithLvHandler(0); // sleep forever and call lv_handler
 }
-
